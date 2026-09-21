107f9b62ec8df90628f09684781e638d31b27fe9
zeynepguzellerr <zeynepguzellerr@gmail.com>
2026-09-22 01:15:44 +0300

    Update lab2_3.c

--- src/lab2_3.c
+++ src/lab2_3.c
@@ -12,8 +12,17 @@
 */
 
 int is_prime(int n) {
-    // TODO: check if n is prime using loop up to sqrt(n)
-    return 0; // placeholder
+    if (n < 2) {
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 int main(void) {
@@ -22,7 +31,19 @@ int main(void) {
     printf("Enter an integer n (>= 2): ");
     scanf("%d", &n);
 
-    // TODO: validate input and print all primes up to n
+    if (n < 2) {
+    printf("Error: n must be at least 2.\n");
+} else {
+    printf("Prime numbers up to %d: ", n);
+
+    for (int i = 2; i <= n; i++) {
+        if (is_prime(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+}
 
     return 0;
 }
