de9b608df8113a85f386ad4576c823172ed67154
zeynepguzellerr <zeynepguzellerr@gmail.com>
2026-09-22 01:13:38 +0300

    Update lab2_2.c

--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,8 +12,13 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+    long long result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result = result * i;
+    }
+
+    return result;
 }
 
 int main(void) {
@@ -22,7 +27,12 @@ int main(void) {
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
 
-    // TODO: validate input, call function, print result
+    if (n < 0) {
+    printf("Error: n must be non-negative.\n");
+} else {
+    long long result = factorial(n);
+    printf("%d! = %lld\n", n, result);
+}
 
     return 0;
 }
