12ee655ad00d0175e7c9c43fca02d9c2339c9dd1
zeynepguzellerr <zeynepguzellerr@gmail.com>
2026-09-21 08:05:00 +0000

    Update hello message

--- src/hello.c
+++ src/hello.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
   // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  printf("Hello from Zeynep!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
